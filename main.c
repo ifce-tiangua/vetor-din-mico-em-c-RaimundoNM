6a75eede4c3dc63333a038f6b378751f8839358f
RaimundoNM <raimundonm091@gmail.com>
2024-11-15 18:43:17 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main() {
+    int *vetor, tamanho, i;
+    scanf("%d", &tamanho);
+    vetor = (int*)malloc(tamanho * sizeof(int));
+    if(vetor == NULL|| tamanho == 0) {
+        printf("[vazio]");
+    } else {
+        for(i = 0; i < tamanho; i++) {
+            scanf("%d", &vetor[i]);
+        }
+        printf("[");
+        for(i = 0; i < tamanho; i++) {
+            printf("%d", vetor[i]);
+            if(i < tamanho - 1) {
+                printf(",");
+            }
 
+
+        }
+        printf("]");
+        free(vetor);
+    }
     return 0;
 }
